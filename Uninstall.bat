1b76580783cc82bfd1627bcefcc0fd7fcb451648
Twan Kok <twankok4@gmail.com>
2026-04-03 23:02:04 +0200

    Fix: remove VBS on uninstall, also clean up old registry entry

--- Uninstall.bat
+++ Uninstall.bat
@@ -1,19 +1,24 @@
 @echo off
+set STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set VBSFILE=%STARTUP%\CrewChiefAutoLauncher.vbs
 set REGKEY=HKCU\Software\Microsoft\Windows\CurrentVersion\Run
 set REGNAME=CrewChiefAutoLauncher
 
 echo Verwijderen van CrewChief Auto-Launcher...
 
-REM Verwijder uit Windows-opstart
+REM Verwijder VBS uit Startup-map
+if exist "%VBSFILE%" del "%VBSFILE%" >nul 2>&1
+
+REM Verwijder ook eventuele registry-entry (van oudere versie)
 reg delete "%REGKEY%" /v "%REGNAME%" /f >nul 2>&1
 
 echo [OK] Verwijderd uit Windows-opstart.
 
 REM Stop eventueel draaiende instantie
 powershell.exe -Command "
-Get-Process powershell -ErrorAction SilentlyContinue |
-Where-Object { (Get-WmiObject Win32_Process -Filter \"ProcessId = \$(\$_.Id)\" -ErrorAction SilentlyContinue).CommandLine -like '*CrewChiefAutoLauncher*' } |
-Stop-Process -Force -ErrorAction SilentlyContinue
+Get-CimInstance Win32_Process -ErrorAction SilentlyContinue |
+Where-Object { $_.CommandLine -like '*CrewChiefAutoLauncher*' } |
+ForEach-Object { Stop-Process -Id $_.ProcessId -Force -ErrorAction SilentlyContinue }
 Write-Output 'Gestopt.'
 " 2>&1
 
